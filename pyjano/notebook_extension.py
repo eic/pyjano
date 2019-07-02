b181fc9e3fdef092786fd428e819f4ae5a5c7013
Dmitry Romanov <romanovda@gmail.com>
2019-07-02 07:25:16 -0400

    Fixed root files server extension

--- pyjano/notebook_extension.py
+++ pyjano/notebook_extension.py
@@ -1,7 +1,17 @@
+import os
+from pprint import pprint
 import tornado.web
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 
+import os
+
+# i am big on absolute paths, so i made it return an absolute path
+# also that makes the results clearer.
+def full_path(dir_):
+    if dir_[0] == '~' and not os.path.exists(dir_):
+        dir_ = os.path.expanduser(dir_)
+    return os.path.abspath(dir_)
 
 class HelloWorldHandler(IPythonHandler):
     def get(self):
@@ -9,6 +19,10 @@ class HelloWorldHandler(IPythonHandler):
 
 
 class CorsStaticFileHandler(tornado.web.StaticFileHandler):
+
+    # def get(self, **other):
+    #     pprint(other)
+    #     return tornado.web.StaticFileHandler.get(**other)
 
     def set_default_headers(self):
         self.set_header("Access-Control-Allow-Origin", "*")
@@ -21,19 +35,22 @@ def load_jupyter_server_extension(nb_app):
 
     Based on https://github.com/Carreau/jupyter-book/blob/master/extensions/server_ext.py
     '''
-    from pprint import pprint
+
 
     web_app = nb_app.web_app
 
     nb_app.log.debug("=" * 30)
-    nb_app.log.debug('Setting Pyjano nb server extension')
+    nb_app.log.info('Setting Pyjano nb server extension')
     nb_app.log.debug(type(web_app))
-    # pprint(vars(nb_app))
+    server_root_dir = full_path(web_app.settings['server_root_dir'])
+    pprint(server_root_dir)
     # pprint(vars(web_app))
-    nb_app.log.debug("=" * 30)
+    print("=" * 30)
+
 
     host_pattern = '.*$'
     #route_pattern = url_path_join(web_app.settings['base_url'], '/hello')
     #web_app.add_handlers(host_pattern, [(route_pattern, tornado.web.StaticFileHandler)])
-    web_app.add_handlers(host_pattern, [(r"/rjs/(.*)", CorsStaticFileHandler, {"path": web_app.settings['server_root_dir'], "default_filename": "root_browser.html"})])
+
+    web_app.add_handlers(host_pattern, [(r"/rjs/(.*)", CorsStaticFileHandler, {"path": server_root_dir, "default_filename": "README.md"})])
     #web_app.add_handlers(host_pattern, [(route_pattern, tornado.web.StaticFileHandler, {"path":r"/home/romanov/ceic/static"})])
